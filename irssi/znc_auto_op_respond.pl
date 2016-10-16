140d3e3eb38a3feee48c8d841e5172680ab27d28
Jesse <the01culb@gmail.com>
2016-10-16 19:57:58 -0400

    Fix issue 3
    Fix issue 3

--- irssi/znc_auto_op_respond.pl
+++ irssi/znc_auto_op_respond.pl
@@ -3,7 +3,7 @@ use warnings;
 use Digest::MD5 qw( md5_hex );
 use Irssi qw( signal_add );
 
-our $VERSION = 1;
+our $VERSION = 2;
 our %IRSSI = (
     authors     => 'culb (nightfrog)',
     contact     => 'the01culb@gmail.com',
@@ -15,7 +15,7 @@ our %IRSSI = (
 signal_add 'event notice' => sub
 {
     my( $server, $data, $nick, $address ) = @_;
-    my( $me, $challenge ) = $data =~ /^(\S+)\s{1}:!ZNCAO\s{1}CHALLENGE\s{1}(.+)$/;
+    my( $me, $challenge ) = $data =~
 
     $server->command(   'NOTICE ' . $nick . ' !ZNCAO RESPONSE '
                       . md5_hex( 'PASSWORD_GOES_HERE' . "::" . $challenge ) );
