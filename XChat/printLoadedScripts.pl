45a417ebccdba3e7d497b50f6f4181162e150f01
culb <the01culb@protonmail.com>
2024-02-10 13:53:34 -0500

    Initial commit
    Initial commit

---
+++ XChat/printLoadedScripts.pl
@@ -0,0 +1,54 @@
+# File: printLoadedScripts.pl
+# Language: Perl
+# Version: 0
+#
+# Author: culb( nightfrog )
+# Contact: the01culb[at]protonmail[dot]com
+#
+# Description: A simple script to print loaded Perl scripts
+# Usage: /printscripts
+#
+# License: If this code is used in any way
+#          then be courteous and include all of my information and notes
+########################################################################
+
+use strict;
+use warnings;
+use Xchat qw( :all );
+
+hook_command(
+    'PRINTSCRIPTS',
+    sub{
+        # Find names ending in :: and remove the ::
+        for my $fileName ( sort grep s!::$!!, keys %Xchat::Script:: )
+        {
+    	    serverTab( get_info 'id' );
+    	    $fileName =~ s!_5f!_!g;
+    	    prnt $fileName;
+        }
+        return EAT_XCHAT;
+    },
+    {
+        help_text => "/printscripts - Print out the loaded Perl scripts to the server tab"
+    }
+);
+
+sub serverTab
+{
+    my $connectionId = shift;
+    for my $tab ( get_list 'channels' )
+    {
+        if ( $tab->{id} == $connectionId && $tab->{type} == 1 )
+        {
+            return set_context( $tab->{context} );
+        }
+    }
+    return;
+}
+
+register(
+    'Print Loaded Scripts',
+    0x01,
+    'Print Loaded Perl Scripts',
+    undef
+);
