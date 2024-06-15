3f0886fe34c56b14f63a258e5d7ab70d2de9b4cb
culb <the01culb@protonmail.com>
2024-06-15 17:29:12 -0400

    Format and indentation changes
    Format and indentation changes

--- XChat/close_specific.py
+++ XChat/close_specific.py
@@ -19,14 +19,14 @@ __module_description__ = 'Close a Dialog by name'
 import xchat
 
 def closepm( word, word_eol, userdata ):
-	for v in xchat.get_list( 'channels' ):
-		if v.type == 3 and v.channel.lower() == word[1].lower():
-			v.context.command( "CLOSE" );
+    for v in xchat.get_list( 'channels' ):
+        if v.type == 3 and v.channel.lower() == word[1].lower():
+            v.context.command( "CLOSE" );
 
-	return xchat.EAT_XCHAT
+    return xchat.EAT_XCHAT
  
 xchat.hook_command(
-	'closepm',
-	closepm,
-	help='closepm NICK'
+    'closepm',
+    closepm,
+    help='closepm NICK'
 )
